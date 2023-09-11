78bf8ee7957755a57a2a17b75478535239c9d9ff
sanng97 <sanng97@naver.com>
2023-09-11 19:49:27 +0900

    0911

--- 03_JDBC_test/jdbc_test1/src/edu/kh/test/model/dao/ShopDAO.java
+++ 03_JDBC_test/jdbc_test1/src/edu/kh/test/model/dao/ShopDAO.java
@@ -20,16 +20,30 @@ public class ShopDAO {
 		
 		try {
 			
-			String sql = "SELECT * FROM GHOP_MEMBER WHERE MEMBER_ID = ? +"
-					+ "MEMBER_PW";
+			// sql에 ?가 있으면 preparedStatement 사용
+			String sql = "SELECT * FROM SHOP_MEMBER WHERE MEMBER_ID = ?";
 			
+			//PreparedStatement 생성
 			pstmt = conn.prepareStatement(sql);
 			
-			if(rs.next()) {
+			//?에 알맞은 값 대입 (누락)
+			pstmt.setString(1,memberId);
+			
+			// sql 수행 후 결과(ResultSet) 반환 (누락)
+			rs= pstmt.executeQuery();
+			
+			// dml인 경우 : executeUpdate() -> int반환
+			
+			
+			
+			if(rs.next()) { // 1행 조회 시 if문 사용이 효율 좋음
+				
 				String id = rs.getString("MEMBER_ID");
 				String pw = rs.getString("MEMBER_PW");
 				String phone = rs.getString("PHONE");
-				String gender = rs.getString("성별");
+				// 조회된 컬럼명과 얻어오려는 컬럼명이 다름 (성별 -> GENDER)
+				String gender = rs.getString("GENDER");
+				sm = new ShopMember(id, pw, phone, gender);
 			}
 			
 			
@@ -42,8 +56,9 @@ public class ShopDAO {
 			
 			
 		}finally {
-			
-			close(conn);
+			// 마지막에 jdbc 객체 자원을 무조건 반환하는 구문 누락
+			close(pstmt);
+			close(rs);
 		}
 		return sm;
 		
