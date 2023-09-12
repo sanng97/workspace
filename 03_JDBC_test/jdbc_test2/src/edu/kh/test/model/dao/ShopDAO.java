1c2566bc3fccf8bfae4abbd17aed3dc1ebdc5888
sanng97 <sanng97@naver.com>
2023-09-12 18:26:53 +0900

    0912

---
+++ 03_JDBC_test/jdbc_test2/src/edu/kh/test/model/dao/ShopDAO.java
@@ -0,0 +1,15 @@
+package edu.kh.test.model.dao;
+
+import static edu.kh.test.common.JDBCTemplate.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ShopDAO {
+
+	private PreparedStatement pstmt = null;
+	
+
+}
+
+
+
