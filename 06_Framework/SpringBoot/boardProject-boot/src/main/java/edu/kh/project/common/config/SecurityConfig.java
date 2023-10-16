bd38c35f85982e6e0fdc36d5119d782777c8a667
sanng97 <sanng97@naver.com>
2023-10-16 17:20:24 +0900

    1016(2)

---
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/common/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package edu.kh.project.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration // 수동 been등록 + 설정하기
+public class SecurityConfig {
+
+	@Bean 
+	public BCryptPasswordEncoder BCryptPasswordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -0,0 +1,56 @@
+package edu.kh.project.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.member.model.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j  //로그
+@Controller //controller역할(요청/응답 제어) + bean 등록
+@RequestMapping("/member")
+@SessionAttributes({"loginMember"})// 세션올리기
+public class MemberController {
+
+	// MemberService 의존성 주입
+	@Autowired
+	private MemberService service;
+	
+     //  / member/login(POST)
+	
+	@PostMapping("login")
+	public String login (Member inputMember,
+					Model model,RedirectAttributes ra){
+		
+		
+		// 로그인 서비스 호출
+		Member loginMember = service.login(inputMember);
+		
+		// 로그인 성공 시
+		if(loginMember != null) {
+			
+			// 쿠키 생성 코드 작성 예정
+		}
+		
+		// 로그인 실패 시
+		if(loginMember == null) {
+			ra.addFlashAttribute("message","아이디 또는 비밀번호가 일치하지 않습니다.");
+
+		}
+		
+		// 로그인 결과를 session scope에 추가
+		model.addAttribute("loginMember",loginMember);
+		
+		
+		// 메인 페이지 리다이렉트
+		return "redirect:/";
+		
+	}
+	 
+}
